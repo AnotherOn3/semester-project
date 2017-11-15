b183b406a939888267e826c2fa38b843e3b04018
aleksandarmihaylov <aleksandaryanev95@gmail.com>
2017-11-15 14:04:51 +0100

    Fixing a bug where reference looping is handled

--- Backend/Another one backend solution/Another one backend/Global.asax.cs
+++ Backend/Another one backend solution/Another one backend/Global.asax.cs
@@ -18,6 +18,10 @@ namespace Another_one_backend
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings
+    .ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            GlobalConfiguration.Configuration.Formatters
+                .Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
         }
     }
 }
