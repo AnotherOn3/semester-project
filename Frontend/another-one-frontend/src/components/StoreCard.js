ba1ed1c9a766c742cb0c335248db403b33bbf7f1
Gintaras Arstikaitis <garstikaitis@gmail.com>
2017-11-16 14:01:14 +0100

    3 Columns in the card is ready to be styled

--- Frontend/another-one-frontend/src/components/StoreCard.js
+++ Frontend/another-one-frontend/src/components/StoreCard.js
@@ -1,26 +1,64 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
 
 const StoreCard = () => {
   return (
     <View style={styles.cardContainer}>
       <View style={styles.cardLeftContainer}>
         <Text>Fakta</Text>
+        <Image
+          style={styles.imageStyle}
+          source={{ uri: 'https://placehold.it/30x30.png' }}
+        />
+        <Text>23 Discounts</Text>
       </View>
-      <View style={styles.cardRightContainer} />
+      <View style={styles.cardMiddleContainer}>
+        <Text>Popular discounts</Text>
+        <View>
+          <Image
+            style={styles.imageStyle}
+            source={{ uri: 'https://placehold.it/30x30.png' }}
+          />
+          <View style={styles.row}>
+            <Text>1</Text>
+            <Text>kg </Text>
+            <Text>oranges</Text>
+          </View>
+          <View style={styles.price}>
+            <Text>20 ,-DKK</Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.cardRightContainer}>
+        <Image
+          style={styles.imageStyle}
+          source={{ uri: 'https://placehold.it/30x30.png' }}
+        />
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   cardContainer: {
-    backgroundColor: 'red',
+    flexDirection: 'row',
   },
-  cardLeftContainer: {
-    backgroundColor: 'blue',
-  },
+  cardLeftContainer: {},
+  cardMiddleContainer: {},
   cardRightContainer: {
     backgroundColor: 'pink',
+    justifyContent: 'center',
+  },
+  imageStyle: {
+    height: 30,
+    width: 30,
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  price: {
+    width: 100,
+    alignItems: 'flex-end',
   },
 });
 
