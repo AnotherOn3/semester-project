0ecf365e6319cb2db9533dfe44e03daa5fd6ab6b
aleksandarmihaylov <aleksandaryanev95@gmail.com>
2017-11-20 14:26:09 +0100

    Adding custom fonts

--- Frontend/another-one-frontend/src/components/PopularProduct.js
+++ Frontend/another-one-frontend/src/components/PopularProduct.js
@@ -1,29 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text, Image, StyleSheet } from 'react-native';
+import { Font } from 'expo';
 
-const PopularProduct = props => {
-  return (
-    <View>
-      <View style={styles.productInfoContainer}>
-        <Image style={styles.imageStyle} source={{ uri: props.imageUrl }} />
-        <View style={styles.productInfo}>
-          <View style={styles.product}>
-            <Text>{props.quantity}</Text>
-            <Text>{props.quantityType} </Text>
-            <Text>{props.productName}</Text>
-          </View>
-          <View style={styles.price}>
-            <Text>{props.price} ,-DKK</Text>
+class PopularProduct extends Component {
+  state = {
+    fontLoaded: false,
+  };
+  async componentDidMount() {
+    await Font.loadAsync([
+      { Light: require('../../assets/fonts/Light.otf') },
+      { Bold: require('../../assets/fonts/Bold.otf') },
+      { Italic: require('../../assets/fonts/Italic.otf') },
+      { Medium: require('../../assets/fonts/Medium.otf') },
+      { Regular: require('../../assets/fonts/Regular.otf') },
+      { SemiBold: require('../../assets/fonts/SemiBold.otf') },
+    ]);
+    this.setState({ fontLoaded: true });
+  }
+  render() {
+    if (this.state.fontLoaded) {
+      return (
+        <View>
+          <View style={styles.productInfoContainer}>
+            <Image
+              style={styles.imageStyle}
+              source={{ uri: this.props.imageUrl }}
+            />
+            <View style={styles.productInfo}>
+              <View style={styles.product}>
+                <Text>{this.props.quantity}</Text>
+                <Text>{this.props.quantityType} </Text>
+                <Text style={styles.text}>{this.props.productName}</Text>
+              </View>
+              <View style={styles.price}>
+                <Text>{this.props.price} ,-DKK</Text>
+              </View>
+            </View>
           </View>
         </View>
-      </View>
-    </View>
-  );
-};
+      );
+    } else {
+      return null;
+    }
+  }
+}
 
 const styles = StyleSheet.create({
   productInfoContainer: {
     flexDirection: 'row',
+    marginBottom: 12,
   },
   productInfo: {
     flexDirection: 'column',
@@ -42,6 +67,9 @@ const styles = StyleSheet.create({
   product: {
     flexDirection: 'row',
     justifyContent: 'flex-end',
+  },
+  text: {
+    fontFamily: 'Regular',
   },
 });
 
